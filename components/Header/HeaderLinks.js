aa8988ec3a6d7efc4c1471c2d53874bd45fae9a1
Mehdi Raza <mr@berkeley-assets.com>
2022-09-13 16:54:16 +0400

    Extra Pages which needs to be deleted later

--- components/Header/HeaderLinks.js
+++ components/Header/HeaderLinks.js
@@ -58,7 +58,7 @@ export default function HeaderLinks(props) {
   const smoothScroll = (e, target) => {
     if (window.location.pathname === "/sections") {
       var isMobile = navigator.userAgent.match(
-        /(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i
+          /(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i
       );
       if (isMobile) {
         // if we are on mobile device the scroll into view will be managed by the browser
@@ -71,9 +71,9 @@ export default function HeaderLinks(props) {
   };
   const scrollGo = (element, to, duration) => {
     var start = element.scrollTop,
-      change = to - start,
-      currentTime = 0,
-      increment = 20;
+        change = to - start,
+        currentTime = 0,
+        increment = 20;
 
     var animateScroll = function () {
       currentTime += increment;
@@ -90,227 +90,227 @@ export default function HeaderLinks(props) {
   const { dropdownHoverColor } = props;
   const classes = useStyles();
   return (
-    <List className={classes.list + " " + classes.mlAuto}>
-      <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          navDropdown
-          hoverColor={dropdownHoverColor}
-          buttonText="Components"
-          buttonProps={{
-            className: classes.navLink,
-            color: "transparent"
-          }}
-          buttonIcon={Apps}
-          dropdownList={[
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/presentation">
-              <a className={classes.dropdownLink}>
-                <LineStyle className={classes.dropdownIcons} /> Presentation
-                Page
-              </a>
-            </Link>,
-            <Link href="/components">
-              <a className={classes.dropdownLink}>
-                <Layers className={classes.dropdownIcons} />
-                All components
-              </a>
-            </Link>,
-            <a
-              href="https://demos.creative-tim.com/nextjs-material-kit-pro/documentation/tutorial?ref=njsmkp-navbar"
-              target="_blank"
-              className={classes.dropdownLink}
+      <List className={classes.list + " " + classes.mlAuto}>
+        <ListItem className={classes.listItem}>
+          <CustomDropdown
+              noLiPadding
+              navDropdown
+              hoverColor={dropdownHoverColor}
+              buttonText="Components"
+              buttonProps={{
+                className: classes.navLink,
+                color: "transparent"
+              }}
+              buttonIcon={Apps}
+              dropdownList={[
+                <Link href="/presentation">
+                  <a className={classes.dropdownLink}>
+                    <LineStyle className={classes.dropdownIcons} /> Presentation
+                    Page
+                  </a>
+                </Link>,
+                <Link href="/components">
+                  <a className={classes.dropdownLink}>
+                    <Layers className={classes.dropdownIcons} />
+                    All components
+                  </a>
+                </Link>,
+                <a
+                    href="https://demos.creative-tim.com/nextjs-material-kit-pro/documentation/tutorial?ref=njsmkp-navbar"
+                    target="_blank"
+                    className={classes.dropdownLink}
+                >
+                  <Icon className={classes.dropdownIcons}>content_paste</Icon>
+                  Documentation
+                </a>
+              ]}
+          />
+        </ListItem>
+        <ListItem className={classes.listItem}>
+          <CustomDropdown
+              noLiPadding
+              navDropdown
+              hoverColor={dropdownHoverColor}
+              buttonText="Sections"
+              buttonProps={{
+                className: classes.navLink,
+                color: "transparent"
+              }}
+              buttonIcon={ViewDay}
+              dropdownList={[
+                <Link href="/sections#headers">
+                  <a
+                      className={classes.dropdownLink}
+                      onClick={(e) => smoothScroll(e, "headers")}
+                  >
+                    <Dns className={classes.dropdownIcons} /> Headers
+                  </a>
+                </Link>,
+                <Link href="/sections#features">
+                  <a
+                      className={classes.dropdownLink}
+                      onClick={(e) => smoothScroll(e, "features")}
+                  >
+                    <Build className={classes.dropdownIcons} /> Features
+                  </a>
+                </Link>,
+                <Link href="/sections#blogs">
+                  <a
+                      className={classes.dropdownLink}
+                      onClick={(e) => smoothScroll(e, "blogs")}
+                  >
+                    <ListIcon className={classes.dropdownIcons} /> Blogs
+                  </a>
+                </Link>,
+                <Link href="/sections#teams">
+                  <a
+                      className={classes.dropdownLink}
+                      onClick={(e) => smoothScroll(e, "teams")}
+                  >
+                    <People className={classes.dropdownIcons} /> Teams
+                  </a>
+                </Link>,
+                <Link href="/sections#projects">
+                  <a
+                      className={classes.dropdownLink}
+                      onClick={(e) => smoothScroll(e, "projects")}
+                  >
+                    <Assignment className={classes.dropdownIcons} /> Projects
+                  </a>
+                </Link>,
+                <Link href="/sections#pricing">
+                  <a
+                      className={classes.dropdownLink}
+                      onClick={(e) => smoothScroll(e, "pricing")}
+                  >
+                    <MonetizationOn className={classes.dropdownIcons} /> Pricing
+                  </a>
+                </Link>,
+                <Link href="/sections#testimonials">
+                  <a
+                      className={classes.dropdownLink}
+                      onClick={(e) => smoothScroll(e, "testimonials")}
+                  >
+                    <Chat className={classes.dropdownIcons} /> Testimonials
+                  </a>
+                </Link>,
+                <Link href="/sections#contacts">
+                  <a
+                      className={classes.dropdownLink}
+                      onClick={(e) => smoothScroll(e, "contacts")}
+                  >
+                    <Call className={classes.dropdownIcons} /> Contacts
+                  </a>
+                </Link>
+              ]}
+          />
+        </ListItem>
+        <ListItem className={classes.listItem}>
+          <CustomDropdown
+              noLiPadding
+              navDropdown
+              hoverColor={dropdownHoverColor}
+              buttonText="Examples"
+              buttonProps={{
+                className: classes.navLink,
+                color: "transparent"
+              }}
+              buttonIcon={ViewCarousel}
+              dropdownList={[
+                <Link href="/about-us">
+                  <a className={classes.dropdownLink}>
+                    <AccountBalance className={classes.dropdownIcons} /> About Us
+                  </a>
+                </Link>,
+                <Link href="/blog-post">
+                  <a className={classes.dropdownLink}>
+                    <ArtTrack className={classes.dropdownIcons} /> Blog Post
+                  </a>
+                </Link>,
+                <Link href="/blog-posts">
+                  <a className={classes.dropdownLink}>
+                    <ViewQuilt className={classes.dropdownIcons} /> Blog Posts
+                  </a>
+                </Link>,
+                <Link href="/contact-us">
+                  <a className={classes.dropdownLink}>
+                    <LocationOn className={classes.dropdownIcons} /> Contact Us
+                  </a>
+                </Link>,
+                <Link href="/landing-page">
+                  <a className={classes.dropdownLink}>
+                    <ViewDay className={classes.dropdownIcons} /> Landing Page
+                  </a>
+                </Link>,
+                <Link href="/login">
+                  <a className={classes.dropdownLink}>
+                    <Fingerprint className={classes.dropdownIcons} /> Login Page
+                  </a>
+                </Link>,
+                <Link href="/pricing">
+                  <a className={classes.dropdownLink}>
+                    <AttachMoney className={classes.dropdownIcons} /> Pricing Page
+                  </a>
+                </Link>,
+                <Link href="/shopping-cart">
+                  <a className={classes.dropdownLink}>
+                    <ShoppingBasket className={classes.dropdownIcons} /> Shopping
+                    Cart
+                  </a>
+                </Link>,
+                <Link href="/ecommerce">
+                  <a className={classes.dropdownLink}>
+                    <Store className={classes.dropdownIcons} /> Ecommerce Page
+                  </a>
+                </Link>,
+                <Link href="/product">
+                  <a className={classes.dropdownLink}>
+                    <ShoppingCart className={classes.dropdownIcons} /> Product Page
+                  </a>
+                </Link>,
+                <Link href="/profile">
+                  <a className={classes.dropdownLink}>
+                    <AccountCircle className={classes.dropdownIcons} /> Profile Page
+                  </a>
+                </Link>,
+                <Link href="/signup">
+                  <a className={classes.dropdownLink}>
+                    <PersonAdd className={classes.dropdownIcons} /> Signup Page
+                  </a>
+                </Link>,
+                <Link href="/error-page">
+                  <a className={classes.dropdownLink}>
+                    <Error className={classes.dropdownIcons} /> Error Page
+                  </a>
+                </Link>
+              ]}
+          />
+        </ListItem>
+        <ListItem className={classes.listItem}>
+          <Hidden mdDown>
+            <Button
+                href="https://www.creative-tim.com/product/nextjs-material-kit-pro?ref=njsmkp-navbar"
+                color={"white"}
+                target="_blank"
+                className={classes.navButton}
+                round
             >
-              <Icon className={classes.dropdownIcons}>content_paste</Icon>
-              Documentation
-            </a>
-          ]}
-        />
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          navDropdown
-          hoverColor={dropdownHoverColor}
-          buttonText="Sections"
-          buttonProps={{
-            className: classes.navLink,
-            color: "transparent"
-          }}
-          buttonIcon={ViewDay}
-          dropdownList={[
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/sections#headers">
-              <a
-                className={classes.dropdownLink}
-                onClick={(e) => smoothScroll(e, "headers")}
-              >
-                <Dns className={classes.dropdownIcons} /> Headers
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/sections#features">
-              <a
-                className={classes.dropdownLink}
-                onClick={(e) => smoothScroll(e, "features")}
-              >
-                <Build className={classes.dropdownIcons} /> Features
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/sections#blogs">
-              <a
-                className={classes.dropdownLink}
-                onClick={(e) => smoothScroll(e, "blogs")}
-              >
-                <ListIcon className={classes.dropdownIcons} /> Blogs
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/sections#teams">
-              <a
-                className={classes.dropdownLink}
-                onClick={(e) => smoothScroll(e, "teams")}
-              >
-                <People className={classes.dropdownIcons} /> Teams
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/sections#projects">
-              <a
-                className={classes.dropdownLink}
-                onClick={(e) => smoothScroll(e, "projects")}
-              >
-                <Assignment className={classes.dropdownIcons} /> Projects
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/sections#pricing">
-              <a
-                className={classes.dropdownLink}
-                onClick={(e) => smoothScroll(e, "pricing")}
-              >
-                <MonetizationOn className={classes.dropdownIcons} /> Pricing
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/sections#testimonials">
-              <a
-                className={classes.dropdownLink}
-                onClick={(e) => smoothScroll(e, "testimonials")}
-              >
-                <Chat className={classes.dropdownIcons} /> Testimonials
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/sections#contacts">
-              <a
-                className={classes.dropdownLink}
-                onClick={(e) => smoothScroll(e, "contacts")}
-              >
-                <Call className={classes.dropdownIcons} /> Contacts
-              </a>
-            </Link>
-          ]}
-        />
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          navDropdown
-          hoverColor={dropdownHoverColor}
-          buttonText="Examples"
-          buttonProps={{
-            className: classes.navLink,
-            color: "transparent"
-          }}
-          buttonIcon={ViewCarousel}
-          dropdownList={[
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/about-us">
-              <a className={classes.dropdownLink}>
-                <AccountBalance className={classes.dropdownIcons} /> About Us
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/blog-post">
-              <a className={classes.dropdownLink}>
-                <ArtTrack className={classes.dropdownIcons} /> Blog Post
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/blog-posts">
-              <a className={classes.dropdownLink}>
-                <ViewQuilt className={classes.dropdownIcons} /> Blog Posts
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/contact-us">
-              <a className={classes.dropdownLink}>
-                <LocationOn className={classes.dropdownIcons} /> Contact Us
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/landing-page">
-              <a className={classes.dropdownLink}>
-                <ViewDay className={classes.dropdownIcons} /> Landing Page
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/login">
-              <a className={classes.dropdownLink}>
-                <Fingerprint className={classes.dropdownIcons} /> Login Page
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/pricing">
-              <a className={classes.dropdownLink}>
-                <AttachMoney className={classes.dropdownIcons} /> Pricing Page
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/shopping-cart">
-              <a className={classes.dropdownLink}>
-                <ShoppingBasket className={classes.dropdownIcons} /> Shopping
-                Cart
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/ecommerce">
-              <a className={classes.dropdownLink}>
-                <Store className={classes.dropdownIcons} /> Ecommerce Page
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/product">
-              <a className={classes.dropdownLink}>
-                <ShoppingCart className={classes.dropdownIcons} /> Product Page
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/profile">
-              <a className={classes.dropdownLink}>
-                <AccountCircle className={classes.dropdownIcons} /> Profile Page
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/signup">
-              <a className={classes.dropdownLink}>
-                <PersonAdd className={classes.dropdownIcons} /> Signup Page
-              </a>
-            </Link>,
-            <Link href="/Users/Mehdi/Documents/nextjs-material-kit-pro-v1.2.1/pages/error-page">
-              <a className={classes.dropdownLink}>
-                <Error className={classes.dropdownIcons} /> Error Page
-              </a>
-            </Link>
-          ]}
-        />
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <Hidden mdDown>
-          <Button
-            href="https://www.creative-tim.com/product/nextjs-material-kit-pro?ref=njsmkp-navbar"
-            color={"white"}
-            target="_blank"
-            className={classes.navButton}
-            round
-          >
-            <ShoppingCart className={classes.icons} /> buy now
-          </Button>
-        </Hidden>
-        <Hidden mdUp>
-          <Button
-            href="https://www.creative-tim.com/product/nextjs-material-kit-pro?ref=njsmkp-navbar"
-            color={"info"}
-            target="_blank"
-            className={classes.navButton}
-            round
-          >
-            <ShoppingCart className={classes.icons} /> buy now
-          </Button>
-        </Hidden>
-      </ListItem>
-    </List>
+              <ShoppingCart className={classes.icons} /> buy now
+            </Button>
+          </Hidden>
+          <Hidden mdUp>
+            <Button
+                href="https://www.creative-tim.com/product/nextjs-material-kit-pro?ref=njsmkp-navbar"
+                color={"info"}
+                target="_blank"
+                className={classes.navButton}
+                round
+            >
+              <ShoppingCart className={classes.icons} /> buy now
+            </Button>
+          </Hidden>
+        </ListItem>
+      </List>
   );
 }
 
